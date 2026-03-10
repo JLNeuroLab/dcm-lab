65fb19c6ff1ca5e38b3bcf5bce845c0aff472ce8
Jacopo Larizza <jacopom.larizza@gmail.com>
2026-03-10 16:59:35 +0100

    work in progress on optimization logic for inference

---
+++ src/dcm/inference/optim.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+from dcm.inference.forward_adapter import ForwardAdapter
+from dcm.inference.likelihoods import gaussian_log_likelihood
+from dcm.inference.priors import gaussian_log_prior
+from dcm.inference.objectives import gaussian_log_posterior
+
+def map_estimation()
